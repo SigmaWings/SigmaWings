6bcaa0a425730570019881fe35211cdc945c5aa5
kalyan babu <34261619+kalyanbabu1@users.noreply.github.com>
2023-07-13 16:46:04 +0530

    Added header and navigation for web and mobile

--- src/Layout/CommonLayout.vue
+++ src/Layout/CommonLayout.vue
@@ -1,16 +1,31 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <SigmaHeader
-      @toggle-drawer="toggleLeftDrawer"
-      @toggle-theme="toggleTheme"
-      :icon="icon"
-    />
+    <SigmaHeader @toggle-drawer="toggleLeftDrawer" @toggle-theme="toggleTheme" :icon="icon" />
     <q-drawer v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+      <q-img src="../assets/profile-pic.jpg" />
+      <div class="q-pa-md bg-grey-2 full-height">
+        <q-list separator padding class=" text-black bg-grey-3">
+          <q-item clickable v-ripple :to="menu.path" exact :active="link === menu.label" @click="link = menu.label"
+            active-class="text-black bg-grey-2" v-for="menu, index of menuItems" :key="index">
+            <q-item-section avatar>
+              <q-icon outline color="teal" :name="menu.icon" />
+            </q-item-section>
+            <q-item-section>{{ menu.label }}</q-item-section>
+            <q-item-section side>
+              <q-icon name="chevron_right" color="teal" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <div class="versionFooter text-caption text-center text-teal"> Version 0.0.1</div>
+      </div>
+
     </q-drawer>
 
     <q-page-container>
+      <div class="q-mt-lg q-pt-lg">
+        
       <router-view />
+      </div>
     </q-page-container>
     <!-- <SigmaFooter /> -->
   </q-layout>
@@ -26,6 +41,24 @@ export default {
     SigmaHeader,
   },
   setup() {
+    const link = ref('Home');
+    const menuItems = ref([
+      {
+        label: 'Home',
+        path: '/',
+        icon: 'home'
+      },
+      {
+        label: 'categories',
+        path: '/categories',
+        icon: 'assignment'
+      },
+      {
+        label: 'About',
+        path: '/about',
+        icon: 'info'
+      }
+    ]);
     const leftDrawerOpen = ref(false);
     const iconStatus = ref(false);
     const icon = ref("light_mode");
@@ -49,7 +82,16 @@ export default {
       //functions
       toggleLeftDrawer,
       toggleTheme,
+      menuItems,
+      link
     };
   },
 };
 </script>
+<style>
+.versionFooter {
+  position: fixed;
+  bottom: 1%;
+  left: 34%;
+}
+</style>
--- src/Layout/Header.vue
+++ src/Layout/Header.vue
@@ -1,15 +1,62 @@
 <template>
-  <!-- <q-header elevated class="bg-primary text-white">
-    <q-toolbar>
-      <q-btn dense flat round icon="menu" @click="toggleDrawerEmits" />
-      <q-btn :icon="icon" flat @click="toggleThemeEmits" />
-      <q-toolbar-title> Sigma-Wings </q-toolbar-title>
-    </q-toolbar>
-  </q-header> -->
-  <q-header bordered class="bg-grey-1">
-    <q-tabs no-caps class="text-black ">
-      <q-route-tab v-for="menu, index of menuItems" :key='index' :to="menu.path" exact replace :label="menu.label"/>
-    </q-tabs>
+  <q-header bordered class="bg-grey-1 text-white mobile-only">
+    <!-- <q-toolbar> -->
+    <div class="row q-px-md items-center">
+      <div class="col-3">
+        <q-btn dense flat round icon="menu" @click="toggleDrawerEmits" color="black" />
+      </div>
+      <div class="col-6">
+        <div class="row items-center justify-center text-black">
+          <div class="text-h6">SIGMA</div>
+          <q-img src="../assets/pngtree-wings-logo-png-image_6554013.png" style="height: 40px; max-width: 80px" />
+        </div>
+      </div>
+      <div class="col-3 text-right"> 
+        <!-- <q-icon name="info" color="red" /> -->
+        <q-icon name="notifications" color="grey" size="sm">
+      </q-icon>
+      </div>
+    </div>
+    <div class="q-px-md q-mb-md q-mt-sm">
+      <q-input label="Search" dense  outlined v-model="text">
+      <template v-slot:prepend>
+        <q-icon name="search" />
+      </template>
+    </q-input>
+    </div>
+    
+    <!-- <q-btn :icon="icon" flat @click="toggleThemeEmits" />
+      <q-toolbar-title> Sigma-Wings </q-toolbar-title> -->
+
+    <!-- </q-toolbar> -->
+  </q-header>
+  <q-header bordered class="bg-grey-1 text-black desktop-only">
+    <div class="row items-center">
+      <div class="col">
+        <q-tabs no-caps class="text-black ">
+          <q-route-tab v-for="menu, index of menuItems" :key='index' :to="menu.path" exact replace :label="menu.label" />
+        </q-tabs>
+      </div>
+      <div class="col text-center">
+        <div class="row items-center justify-center">
+          <div class="text-h6">SIGMA</div>
+          <q-img src="../assets/pngtree-wings-logo-png-image_6554013.png" style="height: 40px; max-width: 110px" />
+        </div>
+      </div>
+      <q-separator vertical />
+      <div class="col">
+        <div class="row justify-end items-center">
+          <q-input label="Search" dense borderless v-model="text">
+            <template v-slot:prepend>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+
+          <q-img src="../assets/profile-pic.jpg" style="height: 50px; max-width: 50px" />
+        </div>
+      </div>
+    </div>
+
     <!-- <q-toolbar>
       <q-toolbar-title class="text-center">
         <q-avatar>
